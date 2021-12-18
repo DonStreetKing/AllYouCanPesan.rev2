cf6f111ca38274c023c9b5a2fdcdc3e95447810c
DonStreetKing <donstreetking@gmail.com>
2021-12-18 21:30:29 +0700

    MainAdapter_Transaksi_ListTransaksi

--- app/src/main/java/com/example/allyoucanpesanrev2/MainAdapter_Transaksi_ListTransaksi.java
+++ app/src/main/java/com/example/allyoucanpesanrev2/MainAdapter_Transaksi_ListTransaksi.java
@@ -5,49 +5,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainAdapter_Transaksi_ListTransaksi extends ArrayAdapter<String> {
+import java.util.List;
 
-    Context context;
-    int[] GambarRestoran;
-    String[] NamaRestoran;
-    String[] TanggalPembelian;
-    String[] JumlahPembelian;
+public class MainAdapter_Transaksi_ListTransaksi extends ArrayAdapter<MainModel_Transaksi_ListTransaksi> {
+    private List<MainModel_Transaksi_ListTransaksi> HistoriTransaksi;
+    private Context context;
 
-    public MainAdapter_Transaksi_ListTransaksi(Context context, String[] NamaRestoran, int[] GambarRestoran, String[] TanggalPembelian, String[] JumlahPembelian) {
-        super(context, R.layout.isi_list_transaksi_di_transaksi,R.id.TextToShow_NamaRestoran, NamaRestoran);
+    public MainAdapter_Transaksi_ListTransaksi(List<MainModel_Transaksi_ListTransaksi> historiTransaksi, Context context) {
+        super(context, R.layout.isi_list_transaksi_di_transaksi, historiTransaksi);
+        this.HistoriTransaksi = historiTransaksi;
         this.context = context;
-        this.GambarRestoran = GambarRestoran;
-        this.NamaRestoran = NamaRestoran;
-        this.TanggalPembelian = TanggalPembelian;
-        this.JumlahPembelian = JumlahPembelian;
     }
+
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        View SingleItem = convertView;
-        MainModel_Transaksi_ListTransaksi holder = null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View listViewItem = inflater.inflate(R.layout.isi_list_transaksi_di_transaksi, null, true);
 
-        if (SingleItem == null){
-            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            SingleItem = layoutInflater.inflate(R.layout.isi_list_transaksi_di_transaksi, parent, false);
-            holder = new MainModel_Transaksi_ListTransaksi(SingleItem);
-            SingleItem.setTag(holder);
-        }
-
-        else {
-            holder = (MainModel_Transaksi_ListTransaksi) SingleItem.getTag();
-        }
-        holder.GambarRestoran.setImageResource(GambarRestoran[position]);
-        holder.NamaRestoran.setText(NamaRestoran[position]);
-        holder.TextToShow_TanggalPembelian.setText(TanggalPembelian[position]);
-        holder.TextToShow_TotalBiaya.setText(JumlahPembelian[position]);
-        SingleItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getContext(),"Anda tekan: "+NamaRestoran[position], Toast.LENGTH_SHORT).show();
-            }
-        });
-        return SingleItem;
+        TextView TextToShow
     }
 }
