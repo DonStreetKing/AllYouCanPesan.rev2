9def22f3532e0bdce925df0f1083a973b362d6a9
DonStreetKing <donstreetking@gmail.com>
2021-12-23 16:40:08 +0700

    Design fixes

--- app/src/main/java/com/example/allyoucanpesanrev2/Akun.java
+++ app/src/main/java/com/example/allyoucanpesanrev2/Akun.java
@@ -1,14 +1,13 @@
 package com.example.allyoucanpesanrev2;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
